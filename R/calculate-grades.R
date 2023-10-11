2221f7fbd6867814c0d05d06a63d6ea0371866f8
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-10-10 21:27:49 -0700

    attempting lateness + drops

--- R/calculate-grades.R
+++ R/calculate-grades.R
@@ -8,7 +8,7 @@
 #' @importFrom tidyr drop_na
 #' @export
 drop_na_assignments <- function(merged_files){
-    merged_files %>%
+    merged_files |>
         tidyr::drop_na(category)
 }
 
@@ -24,15 +24,16 @@ drop_na_assignments <- function(merged_files){
 #' @export
 intermediary_computations <- function(merged_files){
     #num_assigns
-    merged_files %>%
-        dplyr::group_by(sid, category) %>%
-        dplyr::mutate(num_assigns = n()) %>%
-        dplyr::ungroup() %>%
+    merged_files |>
+        dplyr::group_by(sid, category) |>
+        dplyr::mutate(num_assigns = n()) |>
+        dplyr::ungroup() |>
         dplyr::mutate(relevant_assigns = dplyr::case_when(
             is.numeric(drops) ~ num_assigns-drops,
             !is.numeric(drops) ~ 1), #if drop is "min" or "max"
         lateness_min1 = convert_to_min(late_time1),
-        lateness_min2 = convert_to_min(late_time2)
+        lateness_min2 = convert_to_min(late_time2),
+        assign_lateness = convert_to_min(`lateness_(h_m_s)`)
         )
     
 }
@@ -55,9 +56,87 @@ convert_to_min <- function(hms){
 
 
 # - points/score after lateness
-# 
+calculate_score_after_lateness <- function(computed_files) {
+    df_after <- computed_files |>
+        filter(after) |>
+        mutate(points_after_lateness = case_when(
+            #if late_time1 < lateness <= late_time2, scale by late_scale1
+            assign_lateness > lateness_min1 & assign_lateness <= lateness_min2 ~ raw_points*as.numeric(late_scale1),
+            #if late_time2 < lateness, scale by late_scale2
+            assign_lateness > lateness_min2 ~ raw_points*as.numeric(late_scale2),
+            #if lateness <= late_time1, scale by 1
+            TRUE ~ raw_points
+        )) |>
+        mutate(score_after_lateness = points_after_lateness/max_points)
+    
+    df_until <- computed_files |>
+        filter(!after) |>
+        mutate(points_after_lateness = case_when(
+            #if lateness <= late_time1, scale by late_scale2
+            assign_lateness >= lateness_min1 ~ raw_points*as.numeric(late_scale1),
+            #if late_time1 < lateness <= late_time2, scale by late_scale2
+            assign_lateness <= lateness_min2 ~ raw_points*as.numeric(late_scale2),
+            #if late_time2 < lateness, scale by 0
+            TRUE ~ 0
+        ))
+    
+    df_final <- rbind(df_after, df_until) |>
+        arrange(sid)
+}
+
 # - drops (can do min/max too??)
-# 
+
+drop_assignments <- function(computed_files_after_lateness){
+    df_with_drops <- df_with_lateness |>  # default if no drops in class
+        mutate(dropped = FALSE) #all assignments are defaulted to no drops
+    
+    #if drops = 0 OR num_assigns = 1, NO drops
+    
+    #if min, do min calculations
+    
+    #if relevant_assigns <= 0, (i.e. more drops than assignments, OR drops = num_assigns), take max
+    #if max OR relevant_assigns <= 0, relevant_assigns = 1
+    #if relevant_assigns > 0, no change to relevant_assigns
+    
+    
+
+    # df_with_drops <- df_with_drops |>
+    #     mutate(relevant_assigns = case_when(
+    #         relevant_assigns <=0 ~ 1,
+    #         true ~ relevant_assigns
+    #     ))
+    # 
+    # #if drop = "min", take min
+    # drop_min <- df_with_drops |>
+    #     filter(drops = "min") |>
+    #     group_by(sid, category) |>
+    #     arrange(desc(score_after_lateness)) |> #arrange in descending order based on group_by
+    #     slice(1: as.numeric(num_assigns-1)) |> #drop everything except min
+    #     mutate(dropped = TRUE) #these are DROPPED
+    # 
+    # keep_min <- df_with_drops |>
+    #     filter(drops = "min") |>
+    #     group_by(sid, category) |>
+    #     arrange(score_after_lateness) |> #arrange in ascending order based on group_by
+    #     slice(1) |> #keep minimum value
+    #     mutate(dropped = FALSE) #these are KEPT
+    # 
+    # drop_rest <- df_with_drops |>
+    #     filter(drops != "min") |>
+    #     group_by(sid, category) |>
+    #     arrange(score_after_lateness) |> #arrange in ascending order based on group_by
+    #     slice(1) |> #keep minimum value
+    #     mutate(dropped = TRUE) #these are DROPPED
+    # 
+    # keep_rest <- df_with_drops |>
+    #     filter(drops != "min") |>
+    #     group_by(sid, category) |>
+    #     arrange(score_after_lateness) |> #arrange in ascending order based on group_by
+    #     slice(1) |> #keep minimum value
+    #     mutate(dropped = FALSE) #these are KEPT
+    
+    
+}
 # - do category grading by weights
 # 
 # - calculate overall grades
